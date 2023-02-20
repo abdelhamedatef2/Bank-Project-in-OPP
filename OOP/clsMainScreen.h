3a9dc7fd746136933aaf0af6257eb9bbbe95cd1d
root <root@Abdel-hamed>
2023-02-20 22:36:13 +0200

    add delete client screen

--- OOP/clsMainScreen.h
+++ OOP/clsMainScreen.h
@@ -4,6 +4,8 @@
 #include "clsInputValidate.h"
 #include "clsClientListScreen.h"
 #include "clsAddNewClientScreen.h"
+#include "clsDeleteClientScreen.h"
+
 
 using namespace std;
 
@@ -50,7 +52,8 @@ private:
 
     static void _ShowDeleteClientScreen()
     {
-        cout << "\nDelete Client Screen Will be here...\n";
+        //cout << "\nDelete Client Screen Will be here...\n";
+        clsDeleteClientScreen::ShowDeleteClientScreen();
 
     }
 
